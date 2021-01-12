af74b507e9f871901ef6171a7bac57985f488b23
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-12 18:10:45 -0300

    exercicios em python

---
+++ Exe005.py
@@ -0,0 +1,7 @@
+print('Soma de dois numeros')
+num1 = int(input('digite o primeiro numero: '))
+num2 = int(input('digite o segungo numero: '))
+
+soma = num1 + num2
+
+print('A soma entre {} e {} resutltado {}'.format(num1, num2, soma))
