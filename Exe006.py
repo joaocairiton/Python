60c778965d49ebcc6ac336680aa647bd8955c549
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-14 18:30:09 -0300

    exercicios em python

---
+++ Exe006.py
@@ -0,0 +1,8 @@
+n = input('Digite Qualquer coisa: ')
+print('E digito?', n.isdigit())
+print('E alfabetico ?', n.isalpha())
+print('E alfa numerico ?', n.isalnum())
+print('E decimal ?', n.isdecimal())
+print('é letra minuscula', n.islower())
+print('É espaço ?', n.isspace())
+
