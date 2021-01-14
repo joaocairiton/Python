ede7efdeb78c67db59ebc3d01e83cdef009a0689
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-14 20:58:19 -0300

    exercicios em python

---
+++ Exe010.py
@@ -0,0 +1,4 @@
+m1 = int(input('Digita a media 01: '))
+m2 = int(input('Digite a media 02: '))
+resultado = (m1 + m2)/2
+print('Sua media é: {}'.format(resultado))
---
+++ Exe011.py
@@ -0,0 +1,4 @@
+m = float(input('Digite a metragem: '))
+cen = m * 100
+mm = m * 1000
+print('centimetros: {:.0f} \nmilimetros: {:.0f}'.format(cen, mm))
