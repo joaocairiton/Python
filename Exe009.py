72cc74b0539ca854156a60264f7d2b99b6c153e3
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-14 20:24:10 -0300

    exercicios em python

---
+++ Exe009.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um numero: '))
+
+print('resultado do dobro: {}'.format(n*2))
+print('resultado do triplo: {}'.format(n*3))
+print('resultado da Raiz Quadrada: {:.2f}'.format(n**(1/2)))
+
