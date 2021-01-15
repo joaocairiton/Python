559ef48949428647a053608d925b5deab904041d
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-15 12:22:19 -0300

    exercicios em python

--- Exe014.py
+++ Exe014.py
@@ -1,8 +1,8 @@
 altura = float(input('informe a altura: '))
 largura = float(input('inform a largura: '))
 
-res = altura + largura
-tin = res * 2
+res = altura * largura
+tin = res / 2
 
 
 print('altura e a largura da parede: {}\n quantidade de tinta: {}'.format(res, tin))
