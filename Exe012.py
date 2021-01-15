485cd92bf4c85d5e28f6c1028601df2e82cd1df1
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-15 10:34:32 -0300

    exercicios em python

---
+++ Exe012.py
@@ -0,0 +1,9 @@
+def tabuada(multiplicacao):
+    for cont in range(1, 11):
+        print('{0} x {1} = {2}'.format(multiplicacao, cont, multiplicacao * cont))
+
+
+
+if __name__ == '__main__':
+    num = int(input('Digite um número: '))
+    tabuada(num)
