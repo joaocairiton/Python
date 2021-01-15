faf2950ed077a018f43478a90ee02502065e1a68
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-15 15:37:29 -0300

    exercicios em python temperatura

--- Exe015.py
+++ Exe015.py
@@ -3,5 +3,5 @@ ds = float(input('informe o desconto: '))
 desconto =( pr * ds) / 100
 valor_total = pr - desconto
 
-print('mercadoria com produto: {} '.format(valor_total))
+print('mercadoria com produto: {:.2f} '.format(valor_total))
 
---
+++ Exe017.py
@@ -0,0 +1,3 @@
+c = float(input('informe a temperatura em *c : '))
+f = ((9 * c) / 5) + 32
+print('A temperatura em: {}*c e a temperatura em : {}*f'.format(c, f))
