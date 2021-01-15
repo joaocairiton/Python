2c5b02e6b008c6b703de3d724d2fd27a5f2a8f5e
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-15 11:12:10 -0300

    exercicios em python desconto de mercadoria

---
+++ Exe015.py
@@ -0,0 +1,7 @@
+pr = float(input('informe o valor: '))
+ds = float(input('informe o desconto: '))
+desconto =( pr * ds) / 100
+valor_total = pr - desconto
+
+print('mercadoria com produto: {} '.format(valor_total))
+
