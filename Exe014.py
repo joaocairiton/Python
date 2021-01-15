1daba986861c26adf8eb52eb8f6b9dfe2c794dcf
joaocairiton <43797301+joaocairiton@users.noreply.github.com>
2021-01-15 10:59:44 -0300

    exercicios em python tinta

---
+++ Exe014.py
@@ -0,0 +1,8 @@
+altura = float(input('informe a altura: '))
+largura = float(input('inform a largura: '))
+
+res = altura + largura
+tin = res * 2
+
+
+print('altura e a largura da parede: {}\n quantidade de tinta: {}'.format(res, tin))
